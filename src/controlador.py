1bfb1c5174f4460f963e9a7ae52d8a16ebcbba42
Pontax02 <ppontanilla07@gmail.com>
2025-09-05 17:35:20 +0200

    feat(controlador): :sparkles: Implementar metodo que valida los numeros del dni

--- src/controlador.py
+++ src/controlador.py
@@ -1,12 +1,20 @@
-from src.dni import Dni
+from dni import Dni
 
 
 class Controlador:
 
     def __init__(self,dni):
         self.dni = dni
+        
 
     def getDni(self):
+
         return self.dni
-    
-    
+
+    def checkNumbersDni(self):
+        
+        numbers = self.getDni()
+        if len(numbers) == 8:
+            return True
+        else:
+            return False
