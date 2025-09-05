5a6b89a9772214b23634797012848db4ef3bee08
Pontax02 <ppontanilla07@gmail.com>
2025-09-05 17:34:12 +0200

    test(test_dni): :white_check_mark: Actualizar test para comprobar el metodo checknumbersDni

--- test/test_dni.py
+++ test/test_dni.py
@@ -1,9 +1,9 @@
 import pytest
-from src.controlador import Controlador
+from controlador import Controlador
 
 
 def test_dni():
     
     DNI ="09872653"
     test = Controlador(DNI)
-    assert "09872653H" == test.getDNI()
+    assert True== test.checkNumbersDni()
