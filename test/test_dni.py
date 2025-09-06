98851831ba7245741c93d14ac76917950adf0d04
Pontax02 <ppontanilla07@gmail.com>
2025-09-06 13:36:01 +0200

    test(test_dni): :white_check_mark: Corregir casos test

--- test/test_dni.py
+++ test/test_dni.py
@@ -2,8 +2,17 @@ import pytest
 from controlador import Controlador
 
 
-def test_dni():
+def test_numbers_dni():
     
-    DNI ="09872653"
+    DNI ="62007419V"
     test = Controlador(DNI)
     assert True== test.checkNumbersDni()
+
+def test_letter_dni():
+    
+    DNI ="77510958S"
+    test = Controlador(DNI)
+    assert True== test.checkNumbersDni()
+
+[pytest]
+pythonpath = src
