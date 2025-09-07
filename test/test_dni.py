a74a3ec2326f3fa9fef9cd7736145c29eb2e14dc
Pontax02 <ppontanilla07@gmail.com>
2025-09-07 10:45:41 +0200

    test(test_dni): :white_check_mark: Añadir casos test

--- test/test_dni.py
+++ test/test_dni.py
@@ -12,4 +12,16 @@ def test_letter_dni():
     
     DNI ="77510958S"
     test = Controlador(DNI)
-    assert True== test.checkNumbersDni()
+    assert True== test.checkValidLetter()
+
+def test_fake_numbers_dni():
+    
+    DNI ="62-R7419V"
+    test = Controlador(DNI)
+    assert False == test.checkNumbersDni()
+
+def test_fake_letter_dni():
+    
+    DNI ="775109583"
+    test = Controlador(DNI)
+    assert False == test.checkValidLetter()
